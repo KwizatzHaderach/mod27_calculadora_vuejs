3598adae1eae251b43f60da7c97d5a26bb2052ff
Mauricio de Mello <mau-ideias@hotmail.com>
2023-09-19 01:05:53 -0300

    Atualizando para deploy

--- src/components/Total.vue
+++ src/components/Total.vue
@@ -6,7 +6,7 @@
 <template>
     <div class="row">
         <div class="">
-            <p class="">Resultado =</p>
+            <p class="mt-10">Resultado =</p>
             <div class="">
                 <p class="mt-10 fw-bold" v-if="props.filtro === 'soma'">{{ props.somar }}</p>
                 <p class="mt-10 p-2 fw-bold" v-else-if="props.filtro === 'subtracao'">{{ props.subtrair }}</p>
